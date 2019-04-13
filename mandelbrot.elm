518e353e989a6514fbe868fef2dd6ef3921bb874
Linda Zhou <lhmzhou@gmail.com>
2019-04-13 18:31:23 -0500

    include package as dependency to json file

---
+++ mandelbrot.elm
@@ -0,0 +1,7 @@
+module Main exposing (..)
+
+import Html exposing (Attribute, Html, button, div, text)
+import Html.Attributes exposing (style)
+import Html.Events exposing (onClick)
+import Svg exposing (Svg, g, line, svg)
+import Svg.Attributes exposing (stroke, x1, x2, y1, y2)
